6ac9ecdacc678a0dbcc0ea18a7c2d8d90f7aa5a2
Soren-Wei <likefansalt@gmail.com>
2024-10-15 12:32:21 +0800

    修復Bug

--- swiftData1229/SwiftData/DataModel.swift
+++ swiftData1229/SwiftData/DataModel.swift
@@ -14,9 +14,9 @@ import SwiftData
 class DataModel {
     //我們可以使用 @Attribute 屬性包裝器將屬性添加到資料庫的字段中
     //這裡的 .unique 選項告訴我們的資料庫，此列保證是唯一的
-    @Attribute(.unique) let id = UUID()
-    let text: String
-    let priority: Int
+    @Attribute(.unique) private(set) var id = UUID()
+    private(set) var text: String
+    private(set) var priority: Int
     
     init(text: String, priority: Int) {
         self.text = text
